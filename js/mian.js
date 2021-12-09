7a2eb6a1463729e4ce4b4f1b06bebe427238a680
adel-abdelmoniem <adelabdelmoniem2@gmail.com>
2021-12-09 20:39:31 +0300

    Upload The Mian Stracture

--- js/mian.js
+++ js/mian.js
@@ -9,13 +9,23 @@ $('.homeslider').slick({
   autoplay: true
 });
 
+// scroll links
+
+$("a").click(function (e) {
+  let ahref = $(e.target).attr("href");
+  let sectionOffset = $(ahref).offset().top;
+  $("html,body").animate({ scrollTop: sectionOffset-58}, 1500)
+})
+
+
+
 // change navbar background color
 
 let srvsOffset = $("#about").offset().top;
 
 $(window).scroll(function () {
   let sclTop = $(window).scrollTop();
-  if (sclTop > srvsOffset - 5) {
+  if (sclTop > srvsOffset-80) {
     $(".navbar").css("backgroundColor", "rgba(0, 0, 0, 0.3)");
     $(".navbar-light .navbar-nav .nav-link").css("color", "black");
   }
@@ -28,17 +38,8 @@ $(window).scroll(function () {
 
 // btn for scroll top page
 
-$(".footer-btn").click(function () {
+$(".footer-btn,.activeColor").click(function () {
   $("html,body").animate({ scrollTop: 0 }, 2000);
-})
-
-
-// scroll navbar menu
-
-$("a").click(function (e) {
-  let ahref = $(e.target).attr("href");
-  let sectionOffset = $(ahref).offset().top;
-  $("html,body").animate({ scrollTop: sectionOffset - 4 }, 1500)
 })
 
 
@@ -48,5 +49,8 @@ new WOW().init();
 
 
 
-// let ssOffset = $("#services").offset().top;
-// console.log(ssOffset);
+  
+ 
+
+
+
